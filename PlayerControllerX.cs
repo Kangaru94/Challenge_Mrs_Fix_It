334f760f0b64260e8db95ba3eec658940f6c62ce
Kangaru <46432989+Kangaru94@users.noreply.github.com>
2020-03-01 07:25:13 -0600

    Add files via upload

---
+++ PlayerControllerX.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 15f;
+    private float fastSpeed;
+    private float rotationSpeed;
+    private float verticalInput;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+       //fastSpeed is the throttle
+       //rotateSpeed controls rate of plane's turn
+        fastSpeed = speed + 20f;
+        rotationSpeed = 100f;
+
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        //if statement controls throttle based on space bar
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * fastSpeed);
+        }
+
+        //this is the default speed of forward motion
+        else 
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        //rotates the plane 
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed * verticalInput);
+    }
+}
